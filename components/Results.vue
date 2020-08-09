8af0bf9c786207fbb5414aa74ecd68d841e41403
NessMC <contact@nessmc.fr>
2020-08-09 20:19:32 +0100

    🐛 Button position updated.

--- components/Results.vue
+++ components/Results.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="mt-8 mx-4">
+  <div class="mt-8 mx-4 back relative">
     RESULTS COMPONENT
   </div>
 </template>
+
+<style scoped>
+.back {
+  z-index: -1200;
+}
+</style>
