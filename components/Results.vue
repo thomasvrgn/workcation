a017c607b5c8926961ad0d29656a52fc5decdc47
NessMC <contact@nessmc.fr>
2020-08-09 20:19:32 +0100

    💄 Resultst text updated.

--- components/Results.vue
+++ components/Results.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="mt-8 mx-4 back relative">
-    RESULTS COMPONENT
+  <div class="mt-12 mx-8 back relative">
+    <div class="container">
+      <h1 class="text-xl font-semibold text-gray-700">
+        Los Angeles
+      </h1>
+      <p class="text-gray-600">
+        Live like the stars in these luxurious Southern California estates.
+      </p>
+    </div>
   </div>
 </template>
 
