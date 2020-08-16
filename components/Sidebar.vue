dc8a31f7c8da4767c89b577d9b44380c34ca350d
NessMC <contact@nessmc.fr>
2020-08-16 16:38:50 +0200

    🚸 Sidebar interactions added.

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4">
-    <button class="bg-gray-700 text-white font-semibold py-2 px-8 rounded-r-sm focus:outline-none hover:bg-gray-800 focus:bg-gray-900 transition-colors duration-150"  id="sidebarButton">
+    <button class="bg-gray-700 text-white font-semibold py-2 px-8 rounded-r-sm focus:outline-none hover:bg-gray-800 focus:bg-gray-900 transition-colors duration-150" @click=open id="sidebarButton">
       Filters
     </button>
     <div class="absolute inset-0 h-full w-2/6 bg-gray-700 transition duration-500 overflow-y-auto" id="sidebar">
@@ -181,27 +181,27 @@ export default Vue.extend({
   data: () => ({
     opened: false
   }),
-  // mounted: function () {
-  //   const sidebar = document.getElementById('sidebar')
-  //   if (sidebar !== null) {
-  //     sidebar.style.transform = 'translateX(' + -sidebar.offsetWidth + 'px)'
-  //     window.addEventListener('click', function (e) {
-  //       if (e.target !== null) {
-  //         if (!['sidebarButton', 'sidebar'].includes(e.target.id)) {
-  //           sidebar.style.transform = 'translateX(' + -sidebar.offsetWidth + 'px)'
-  //         }
-  //       }
-  //     })
-  //   }
-  // },
-  // methods: {
-  //   open: function () {
-  //     const sidebar = document.getElementById('sidebar')
-  //     if (sidebar !== null) {
-  //       sidebar.style.transform = 'translateX(0)'
-  //     }
-  //   }
-  // }
+  mounted: function () {
+    const sidebar = document.getElementById('sidebar')
+    if (sidebar !== null) {
+      sidebar.style.transform = 'translateX(' + -sidebar.offsetWidth + 'px)'
+      window.addEventListener('click', function (e) {
+        if (e.target !== null) {
+          if (!['sidebarButton', 'sidebar'].includes(e.target.id)) {
+            sidebar.style.transform = 'translateX(' + -sidebar.offsetWidth + 'px)'
+          }
+        }
+      })
+    }
+  },
+  methods: {
+    open: function () {
+      const sidebar = document.getElementById('sidebar')
+      if (sidebar !== null) {
+        sidebar.style.transform = 'translateX(0)'
+      }
+    }
+  }
 })
 </script>
 
