f57efdc71abc07133e54de8f65b9d8ef70fc1ee0
NessMC <contact@nessmc.fr>
2020-08-16 15:34:53 +0200

    🚸 Navbar interactions added.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav class="bg-white pt-4">
+  <nav class="bg-white pt-4" :class="{'py-4': !opened}">
     <div class="container flex flex-row flex-wrap mx-auto">
       <div class="flex flex-auto w-1/2 items-center">
         <div class="container flex-row flex">
-          <span class="flex items-center mr-6 cursor-pointer text-gray-800">
+          <span class="flex items-center mr-6 cursor-pointer text-gray-800" @click=openNavbar>
             <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6Z" fill="currentColor" /><path d="M2 12.0322C2 11.4799 2.44772 11.0322 3 11.0322H21C21.5523 11.0322 22 11.4799 22 12.0322C22 12.5845 21.5523 13.0322 21 13.0322H3C2.44772 13.0322 2 12.5845 2 12.0322Z" fill="currentColor" /><path d="M3 17.0645C2.44772 17.0645 2 17.5122 2 18.0645C2 18.6167 2.44772 19.0645 3 19.0645H21C21.5523 19.0645 22 18.6167 22 18.0645C22 17.5122 21.5523 17.0645 21 17.0645H3Z" fill="currentColor" /></svg>
           </span>
           <span class="bg-gray-100 flex items-center text-gray-500 w-10/12 rounded-md">
@@ -12,7 +12,7 @@
           </span>
         </div>
       </div>
-      <div class="flex flex-auto flex-col space-y-2 order-last list-none my-5 font-medium text-gray-800">
+      <div class="flex flex-auto flex-col space-y-2 order-last list-none my-5 font-medium text-gray-800" :class="{'hidden': !opened}">
         <li class="cursor-pointer hover:text-gray-900 hover:bg-gray-100 px-4 py-2 rounded-md">
           List your property
         </li>
@@ -32,3 +32,17 @@
     </div>
   </nav>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => ({
+    opened: false
+  }),
+  methods: {
+    openNavbar: function () : void {
+      this.opened = !this.opened
+    }
+  }
+})
+</script>
